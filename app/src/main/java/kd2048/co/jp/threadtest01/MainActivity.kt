eb73cde2a0d07c7b082dd606bba335bb12995707
Akira Kobayashi <kuppy777@ari.bekkoame.ne.jp>
2020-05-24 17:25:57 +0900

    少し改造

--- app/src/main/java/kd2048/co/jp/threadtest01/MainActivity.kt
+++ app/src/main/java/kd2048/co/jp/threadtest01/MainActivity.kt
@@ -5,15 +5,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.util.Log
 import android.view.WindowManager
 import android.widget.Button
-import kd2048.co.jp.threadtest01.myHandler.Companion.MESSAGE1
-import kd2048.co.jp.threadtest01.myHandler.Companion.MESSAGE2
-import kd2048.co.jp.threadtest01.myHandler.Companion.MESSAGE3
-import kd2048.co.jp.threadtest01.myHandler.Companion.MESSAGE4
-import java.lang.Thread.currentThread
 import java.lang.Thread.sleep
-import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
+    companion object{
+        val MESSAGE1 = 1
+        val MESSAGE2 = 2
+        val MESSAGE3 = 3
+        val MESSAGE4 = 4
+    }
 
     val mthreadAAA = HandlerThread("AAA")
     val mthreadBBB = HandlerThread("BBB")
@@ -21,7 +21,19 @@ class MainActivity : AppCompatActivity() {
     var mHandlerAAA = Handler()
     var mHandlerBBB = Handler()
     var mHandlerCCC = Handler()
-    var mHandlerMY = myHandler()
+
+    val mHandler = object : Handler(Looper.getMainLooper()) {
+        override fun handleMessage(msg: Message?) {
+            super.handleMessage(msg)
+            when(msg?.what){
+
+                MESSAGE1 -> Log.d("kd>", "MESSAGE1 ${HandlerThread.currentThread()}")
+                MESSAGE2 -> Log.d("kd>", "MESSAGE2 ${HandlerThread.currentThread()}")
+                MESSAGE3 -> Log.d("kd>", "MESSAGE3 ${HandlerThread.currentThread()}")
+                MESSAGE4 -> Log.d("kd>", "MESSAGE4 ${HandlerThread.currentThread()}")
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         // super.xxx()で親クラスで初期化の処理をするので消してはいけない
@@ -43,51 +55,50 @@ class MainActivity : AppCompatActivity() {
         button1.setOnClickListener {
             val r = Runnable {
                 Log.d("kd>", "スレッド開始 ${HandlerThread.currentThread()}")
-                sleep(5000)
+                sleep(1000)
                 Log.d("kd>", "スレッド完了 ${HandlerThread.currentThread()}")
-                mHandlerMY.sendEmptyMessage(MESSAGE1)
+                mHandler.sendEmptyMessage(MESSAGE1)
             }
             Log.d("kd>", "ボタン１クリック")
             mHandlerAAA.post(r)
-            mHandlerBBB.post(r)
-            mHandlerCCC.post(r)
-
         }
+
         val button2 = findViewById<Button>(R.id.button2)
         button2.setOnClickListener {
+            val r = Runnable {
+                Log.d("kd>", "スレッド開始 ${HandlerThread.currentThread()}")
+                sleep(2000)
+                Log.d("kd>", "スレッド完了 ${HandlerThread.currentThread()}")
+                mHandler.sendEmptyMessage(MESSAGE2)
+            }
             Log.d("kd>", "ボタン２クリック")
-            mHandlerMY.sendEmptyMessage(MESSAGE2)
+            mHandlerBBB.post(r)
         }
+
         val button3 = findViewById<Button>(R.id.button3)
         button3.setOnClickListener {
+            val r = Runnable {
+                Log.d("kd>", "スレッド開始 ${HandlerThread.currentThread()}")
+                sleep(3000)
+                Log.d("kd>", "スレッド完了 ${HandlerThread.currentThread()}")
+                mHandler.sendEmptyMessage(MESSAGE3)
+            }
             Log.d("kd>", "ボタン３クリック")
-            mHandlerMY.sendEmptyMessage(MESSAGE3)
+            mHandlerCCC.post(r)
         }
+
         val button4 = findViewById<Button>(R.id.button4)
         button4.setOnClickListener {
+            val r = Runnable {
+                Log.d("kd>", "スレッド開始 ${HandlerThread.currentThread()}")
+                sleep(3000)
+                Log.d("kd>", "スレッド完了 ${HandlerThread.currentThread()}")
+                mHandler.sendEmptyMessage(MESSAGE4)
+            }
             Log.d("kd>", "ボタン４クリック")
-            mHandlerMY.sendEmptyMessage(MESSAGE4)
+            mHandlerAAA.post(r)
+            mHandlerBBB.post(r)
+            mHandlerCCC.post(r)
         }
     }
-}
-
-class myHandler : Handler() {
-    companion object{
-        val MESSAGE1 = 1
-        val MESSAGE2 = 2
-        val MESSAGE3 = 3
-        val MESSAGE4 = 4
-    }
-
-    override fun handleMessage(msg: Message?) {
-        //super.handleMessage(msg)
-        when(msg?.what){
-
-            MESSAGE1 -> Log.d("kd>", "MESSAGE1 ${HandlerThread.currentThread()}")
-            MESSAGE2 -> Log.d("kd>", "MESSAGE2 ${HandlerThread.currentThread()}")
-            MESSAGE3 -> Log.d("kd>", "MESSAGE3 ${HandlerThread.currentThread()}")
-            MESSAGE4 -> Log.d("kd>", "MESSAGE4 ${HandlerThread.currentThread()}")
-        }
-    }
-
 }
